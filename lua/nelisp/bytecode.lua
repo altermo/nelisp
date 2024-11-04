468bf99a5d2e7cc5827094fd4eff82cf76f07285
altermo <107814000+altermo@users.noreply.github.com>
2024-11-04 16:34:41 +0100

    feat: implement more bytecodes

--- lua/nelisp/bytecode.lua
+++ lua/nelisp/bytecode.lua
@@ -639,6 +639,10 @@ function M.exec_byte_code(fun,args_template,args)
                     local v1=pop()
                     set_top(vars.F.string_equal(top(),v1))
                     goto next
+                elseif op==ins.equal then
+                    local v1=pop()
+                    set_top(vars.F.equal(top(),v1))
+                    goto next
                 elseif op==ins.nthcdr then
                     local v1=pop()
                     set_top(vars.F.nthcdr(top(),v1))
