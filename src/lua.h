c6d511cdc57fa6d2504ac1e4645165cea73bba64
altermo <107814000+altermo@users.noreply.github.com>
2025-04-22 08:55:57 +0200

    refactor: no need for no return

--- src/lua.h
+++ src/lua.h
@@ -3,22 +3,33 @@
 
 #include "lisp.h"
 
+struct _lua_assertchange
+{
+  struct lua_State *state;
+  int change;
+};
+
+void
+_lua_assertchange (struct _lua_assertchange *assertchange)
+{
+  TODO_NELISP_LATER;
+  if (lua_gettop (assertchange->state) != assertchange->change)
+    TODO;
+}
+
 // Sets the variable `L` to the global lua-state.
 // The lua-state stack capacity is grown by stack.top+{n} elements.
 // Asserts that the lua-state stack didn't change after run.
-// WARNING: DO NOT RETURN OR JUMP OUT OF THE MACRO.
 #define LUA(n) LUAC (n, 0)
 
 // {L} is the lua-state to be used.
 // The lua-state stack capacity is grown by stack.top+{n} elements.
 // Asserts that the lua-state stack didn't change after run.
-// WARNING: DO NOT RETURN OR JUMP OUT OF THE MACRO.
 #define LUAL(L, n) LUALC (L, n, 0)
 
 // Sets the variable `L` to the global lua-state.
 // The lua-state stack capacity is grown by stack.top+{n} elements.
 // Asserts that the lua-state stack changed by {change}
-// WARNING: DO NOT RETURN OR JUMP OUT OF THE MACRO.
 #define LUAC(n, change)                \
   LUALC (_global_lua_state, n, change) \
   for (lua_State *L = _global_lua_state; run; run = 0)
@@ -26,11 +37,16 @@
 // {L} is the lua-state to be used.
 // The lua-state stack capacity is grown by stack.top+{n} elements.
 // Asserts that the lua-state stack changed by {change}
-// WARNING: DO NOT RETURN OR JUMP OUT OF THE MACRO.
-#define LUALC(L, n, change)                                      \
-  for (int top = (STATIC_ASSERT (n > 0, n_needs_to_be_positive), \
-                  _lcheckstack (L, n), lua_gettop (L) + change), \
-           run = 1;                                              \
-       (eassert (run || top == lua_gettop (L)), run); run = 0)
+#define LUALC(L, n, change)                                              \
+  for (int _top = (STATIC_ASSERT (n > 0, n_needs_to_be_positive),        \
+                   _lcheckstack (L, n), lua_gettop (L) + change),        \
+           run = 1;                                                      \
+       run; run = 0)                                                     \
+    for (__attribute__ ((unused,                                         \
+                         cleanup (                                       \
+                           _lua_assertchange))) struct _lua_assertchange \
+           _assertchange                                                 \
+         = { L, _top };                                                  \
+         run; run = 0)
 
 #endif
