2551146cbb7f99cc4ccde67a3e2deeb1c20c5b2c
altermo <107814000+altermo@users.noreply.github.com>
2025-06-27 13:21:55 +0200

    feat: add all syms of font.c

--- src/font.c
+++ src/font.c
@@ -1,4 +1,5 @@
 #include "font.h"
+#include "lisp.h"
 
 enum xlfd_field_index
 {
@@ -48,4 +49,44 @@ syms_of_font (void)
   sort_shift_bits[FONT_WEIGHT_INDEX] = 9;
   sort_shift_bits[FONT_SIZE_INDEX] = 16;
   sort_shift_bits[FONT_WIDTH_INDEX] = 23;
+
+  DEFSYM (Qopentype, "opentype");
+
+  DEFSYM (Qcanonical_combining_class, "canonical-combining-class");
+
+  DEFSYM (Qascii_0, "ascii-0");
+  DEFSYM (Qiso8859_1, "iso8859-1");
+  DEFSYM (Qiso10646_1, "iso10646-1");
+  DEFSYM (Qunicode_bmp, "unicode-bmp");
+  DEFSYM (Qemoji, "emoji");
+
+  DEFSYM (QCotf, ":otf");
+  DEFSYM (QClang, ":lang");
+  DEFSYM (QCscript, ":script");
+  DEFSYM (QCantialias, ":antialias");
+  DEFSYM (QCfoundry, ":foundry");
+  DEFSYM (QCadstyle, ":adstyle");
+  DEFSYM (QCregistry, ":registry");
+  DEFSYM (QCspacing, ":spacing");
+  DEFSYM (QCdpi, ":dpi");
+  DEFSYM (QCscalable, ":scalable");
+  DEFSYM (QCavgwidth, ":avgwidth");
+  DEFSYM (QCfont_entity, ":font-entity");
+  DEFSYM (QCcombining_capability, ":combining-capability");
+
+  DEFSYM (Qc, "c");
+  DEFSYM (Qm, "m");
+  DEFSYM (Qp, "p");
+  DEFSYM (Qd, "d");
+
+  DEFSYM (Qja, "ja");
+  DEFSYM (Qko, "ko");
+
+  DEFSYM (QCuser_spec, ":user-spec");
+
+  DEFSYM (QL2R, "L2R");
+  DEFSYM (QR2L, "R2L");
+
+  DEFSYM (Qfont_extra_type, "font-extra-type");
+  DEFSYM (Qfont_driver_superseded_by, "font-driver-superseded-by");
 }
