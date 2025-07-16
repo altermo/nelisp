50ce58970c79d3ada703f9bcdcc168dfcc1c9c8b
altermo <107814000+altermo@users.noreply.github.com>
2025-07-16 06:42:37 +0200

    feat: add var translation-table-vector

--- src/character.c
+++ src/character.c
@@ -487,6 +487,13 @@ syms_of_character (void)
   defsubr (&Sstring);
   defsubr (&Sunibyte_string);
 
+  DEFVAR_LISP ("translation-table-vector",  Vtranslation_table_vector,
+        doc: /*
+Vector recording all translation tables ever defined.
+Each element is a pair (SYMBOL . TABLE) relating the table to the
+symbol naming it.  The ID of a translation table is an index into this vector.  */);
+  Vtranslation_table_vector = make_nil_vector (16);
+
   DEFVAR_LISP ("auto-fill-chars", Vauto_fill_chars,
         doc: /*
 A char-table for characters which invoke auto-filling.
